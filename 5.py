142acf7d6329b64efeff3771a522c02251f820cc
ndarrend <95519600+ndarrend@users.noreply.github.com>
2025-12-22 14:07:56 +0800

    Add files via upload

--- 5.py
+++ 5.py
@@ -1,5 +1,22 @@
-bmi= float(input("Enter BMI:"))
-category = 'Normal' if bmi < 25 else(
-'Overweight' if bmi<29.9 else 'Obese'
-)
-print(f"Category:{category}")
+a = float(input("Input first side: "))
+b = float(input("Input second side: "))
+c = float(input("Input third side: "))
+
+if a + b > c and a + c > b and b + c > a:
+
+    longest = max(a, b, c)
+
+    if longest == a:
+        side1, side2 = b, c
+    elif longest == b:
+        side1, side2 = a, c
+    else:
+        side1, side2 = a, b
+
+    if abs(side1**2 + side2**2 - longest**2) < 1e-6:
+        print("The triangle is a Right triangle.")
+    else:
+        print("The triangle is not a Right triangle.")
+
+else:
+    print("The triangle is not valid.")
